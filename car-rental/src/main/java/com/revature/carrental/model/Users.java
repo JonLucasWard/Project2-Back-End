0494cfc9554da079d163c899213fd351a1fee16e
unknown <galadionel@gmail.com>
2019-08-11 16:27:38 -0400

    First Upload

---
+++ car-rental/src/main/java/com/revature/carrental/model/Users.java
@@ -0,0 +1,131 @@
+package com.revature.carrental.model;
+
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+@Table(name="users")
+@EntityListeners(AuditingEntityListener.class)
+public class Users {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(nullable = false)
+	private Long userid;
+	
+	@Column(nullable = false)
+	private String username;
+	
+	@Column(nullable = false)
+	private String password;
+	
+	@Column(nullable = false)
+	private String firstname;
+	
+	@Column(nullable = false)
+	private String lastname;
+	
+	@Column(nullable = false)
+	private String email;
+	
+	@Column(nullable = false)
+	private String phonenumber;
+	
+	@Column(nullable = false)
+	private String driverlicense;
+	
+	@Column(nullable = false)
+	private Long role;
+
+	public Long getUserid() {
+		return userid;
+	}
+
+	public void setUserid(Long userid) {
+		this.userid = userid;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhonenumber() {
+		return phonenumber;
+	}
+
+	public void setPhonenumber(String phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+
+	public String getDriverlicense() {
+		return driverlicense;
+	}
+
+	public void setDriverlicense(String driverlicense) {
+		this.driverlicense = driverlicense;
+	}
+
+	public Long getRole() {
+		return role;
+	}
+
+	public void setRole(Long role) {
+		this.role = role;
+	}
+
+	@Override
+	public String toString() {
+		return "Users [userid=" + userid + ", username=" + username + ", password=" + password + ", firstname="
+				+ firstname + ", lastname=" + lastname + ", email=" + email + ", phonenumber=" + phonenumber
+				+ ", driverlicense=" + driverlicense + ", role=" + role + "]";
+	}
+
+	
+}
